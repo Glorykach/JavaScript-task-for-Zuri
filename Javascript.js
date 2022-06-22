9363ea5d5450f614971893c9012b11c4cecd4541
Onyeka Glory❤️ <88969626+Glorykach@users.noreply.github.com>
2022-06-22 18:12:20 +0100

    Add files via upload

---
+++ Javascript.js
@@ -0,0 +1,9 @@
+var firstName = 'Onyeka';
+var middleName = 'Glory';
+var lastName = 'Onwubalili';
+var height = '1.68m'
+var country = 'Nigeria'
+
+var NAME = firstName + ' ' + middleName + ' ' + lastName;
+
+alert('Name:' + ' ' + NAME + "\n" + 'Height:' + ' ' + height + "\n" + 'Country:' + ' ' + country );
